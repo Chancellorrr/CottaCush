1100d6b4796a6f9445b44c4c1cedd2da7abf8791
Arowo Victor <arowovici@gmail.com>
2019-06-25 16:34:33 +0100

    Add project files.

---
+++ CottaCush/CamelBananaProblem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CottaCush
+{
+    class CamelBananaProblem
+    {
+        public CamelBananaProblem(int initialBananaCount = 3000, int maxLoadPerCamel = 1000, int numberOfCamels = 1, int totalDistance = 1000)
+        {
+            InitialBananaCount = initialBananaCount;
+            CurrentBananaCount = initialBananaCount;
+            MaxLoadPerCamel = maxLoadPerCamel;
+            NumberOfCamels = numberOfCamels;
+            TotalDistance = totalDistance;
+        }
+
+        public int InitialBananaCount { get; set; }
+        public int CurrentBananaCount { get; set; }
+        public int BananasAtMarket { get; set; } = 0;
+        public int DistanceCovered { get; set; } = 0;
+        public int MaxLoadPerCamel { get; set; }
+        public int NumberOfCamels { get; set; }
+        public int TotalDistance { get; set; }
+
+        public int CalculateBananasAtMarket()
+        {
+            int adjustedCamel = NumberOfCamels > (InitialBananaCount / MaxLoadPerCamel) ? CurrentBananaCount / MaxLoadPerCamel : NumberOfCamels;
+            while (true)
+            {
+                int noOfTrips = ((CurrentBananaCount / MaxLoadPerCamel) * 2) - adjustedCamel;
+                DistanceCovered += MaxLoadPerCamel / noOfTrips;
+                CurrentBananaCount -= MaxLoadPerCamel;
+                if (CurrentBananaCount <= MaxLoadPerCamel)
+                {
+                    BananasAtMarket = CurrentBananaCount - (TotalDistance - DistanceCovered);
+                    break;
+                }
+            }
+            return BananasAtMarket;
+
+        }
+
+    }
+}
---
+++ CottaCush/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CottaCush
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            var problem = new CamelBananaProblem();
+            int currentCamelCount = 1;
+            int maxBananas = problem.CalculateBananasAtMarket();
+            int maxCamels = 0;
+
+
+            while (currentCamelCount < 10)
+            {
+                
+                problem = new CamelBananaProblem();
+                currentCamelCount++;
+                problem.NumberOfCamels = currentCamelCount;
+                int current = problem.CalculateBananasAtMarket();
+                if (current > maxBananas)
+                {
+                    maxBananas = current;
+                    maxCamels = problem.NumberOfCamels;
+                }
+            }
+
+            Console.WriteLine(maxBananas + " " + maxCamels);
+        }
+
+
+    }
+}
